4b3eb35f13afbf741a5b2440ab6628c0879b4d57
Yukirelia <ltpcoder2k4@gmail.com>
2022-12-04 09:06:51 +0700

    adjust return type

--- pages/api/readNumber.js
+++ pages/api/readNumber.js
@@ -1,5 +1,10 @@
 function readNumber(num){
-    return num
+    let formattedNumber = []
+    let i = 0
+    for (i = 1; i * 3 <= num.length; i++){
+        formattedNumber.push(num.substring(3 * (i - 1), 3 * i))
+    }
+    return formattedNumber
 }
 
 export default function handler(req, res) {
