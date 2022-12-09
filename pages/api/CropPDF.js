9dd5d3ab2b059280cf34f017682b07d0ea7f31e8
Yukirelia <ltpcoder2k4@gmail.com>
2022-12-09 14:28:19 +0700

    fix crop

--- pages/api/CropPDF.js
+++ pages/api/CropPDF.js
@@ -2,7 +2,7 @@ const {spawn} = require("child_process")
 
 export default function handler(req, res) {
     if (req.method === "POST"){
-        const cropProgram = spawn("python", ['/crop.py', req.body.file])
+        const cropProgram = spawn("python", ['crop.py', req.body.file])
 
         cropProgram.stdout.on('data', (data)=>{
             res.status(200).json({"status": "window.location.origin/" + data})
