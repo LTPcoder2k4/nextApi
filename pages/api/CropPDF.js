5b14222154a94f5fd986ad64bafe0232f04420d3
Yukirelia <ltpcoder2k4@gmail.com>
2022-12-10 09:04:10 +0700

    change method of get file crop

--- pages/api/CropPDF.js
+++ pages/api/CropPDF.js
@@ -1,50 +1,30 @@
-import { spawn } from "child_process"
+import {PDFDocument, rgb} from 'pdf-lib'
 const fs = require('fs')
-
-const content = `
-from PyPDF2 import PdfFileReader, PdfFileWriter
-import requests
-import sys
-            
-url = sys.argv[1]
-            
-response = requests.get(url)
-fileName = response.url[url.rfind('/') + 1:]
-            
-with open(fileName, 'wb') as f:
-    for chunk in response.iter_content(chunk_size=8129):
-        if chunk:
-            f.write(chunk)
-            
-reader = PdfFileReader(fileName, 'r')
-            
-writer = PdfFileWriter()
-            
-for i in range(reader.getNumPages()):
-    page = reader.getPage(i)
-    page.cropBox.setLowerLeft((0, 60))
-    writer.addPage(page)
-            
-outstream = open("result_" + fileName, 'wb')
-writer.write(outstream)
-outstream.close()
-            
-print("result_" + fileName, end='')`
 
 export default function handler(req, res) {
     if (req.method === "POST"){
-        return new Promise((resolve, reject) => {
-            try {
-                fs.writeFileSync("\\crop.py", content)
+        return new Promise((resolve, rejects)=>{
+            fetch(req.body.file)
+            .then(res => res.arrayBuffer())
+            .then(async file =>{
+                const pdfDoc = await PDFDocument.load(file)
 
-                const cropProgram = spawn("python", ["\\crop.py", req.body.file])
-            
-                cropProgram.stdout.on('data', (data)=>{
-                    resolve(res.status(200).json({"file": data.toString()}))
-                })
-            } catch (err) {
-                console.error(err)
-            }
+                const pages = pdfDoc.getPages()
+
+                for (let page of pages){
+                    const svgPath =
+                    `M0,0 ${page.getWidth()},0 ${page.getWidth()},50 0,50`
+    
+                    page.moveTo(0, 60)
+                    page.drawSvgPath(svgPath, { color: rgb(1, 1, 1) })
+                }
+
+                fs.writeFileSync('public/' + req.body.file.substring(req.body.file.lastIndexOf('/'), req.body.file.length), await pdfDoc.save()) 
+                
+                resolve(res.status(200).json({"result": req.body.file.substring(req.body.file.lastIndexOf('/'), req.body.file.length)}))
+            })
         })
+
+        
     }
 }
